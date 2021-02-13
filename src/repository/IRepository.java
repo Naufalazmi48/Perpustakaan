b793e65afc5b0bab187d2ba1d7b35c7c36a822cd
Lalu Naufal Azmi <57085335+Naufalazmi48@users.noreply.github.com>
2021-02-13 08:28:16 +0700

    Update IRepository.java

--- src/repository/IRepository.java
+++ src/repository/IRepository.java
@@ -18,26 +18,4 @@ public interface IRepository<T> {
     boolean create(T data);
     boolean update(T newData, int oldData);
     boolean delete(int id);
-
-//    public User searchUser(int nim);
-//
-
-//
-//    public List<User> showAllUser();
-//
-//    public boolean createUser(User user);
-//
-//    public boolean editUser(User user, int oldNim);
-//
-//    public boolean deleteUser(int nim);
-//
-//    public void login(String username, String password);
-//
-
-//
-
-//
-//    public List<History> getHistory();
-//
-//    public long countTimeLate(int nim, int idBook);
 }
